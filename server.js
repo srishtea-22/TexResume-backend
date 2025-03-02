8fa97712f43cf65a769a619d6fc8026251c8534d
shristy <shristychaudhary4679@gmail.com>
2025-03-02 11:51:31 +0530

    Add latex pdf generation endpoint

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const { exec } = require('child_process');
 const app = express();
 const corsOptions = {
     origin: [ "http://127.0.0.1:5173", "http://localhost:5173"]
@@ -8,8 +10,22 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send( "jai mata di, chal gya");
+app.get("/generate-pdf", (req, res) => {
+    const latexCode = `
+    \\documentclass{article}
+    \\begin{document}
+    Hello, \\textbf{Shirsty}! Your LaTeX PDF works!
+    \\end{document}`;
+
+    fs.writeFileSync("template.tex", latexCode);
+
+    exec("pdflatex template.tex", (error, stdout, stderr) => {
+        if (error) {
+            console.error(`error: ${stderr}`);
+            return res.status(500).send("pdf generation failed");
+        }
+        res.sendFile(__dirname + "/template.pdf");
+    })
 });
 
 app.listen(5000, () => {
